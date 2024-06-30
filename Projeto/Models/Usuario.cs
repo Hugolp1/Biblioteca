1a322a674ebeb90258a4103ecc30ee5530464401
gabrprog <g.alberto.dev@gmail.com>
2024-06-30 13:45:58 -0300

    classe alterada

--- Projeto/Models/Usuario.cs
+++ Projeto/Models/Usuario.cs
@@ -1,9 +1,19 @@
+using System.Collections.Generic;
+
 namespace MyProject.Models
 {
     public class Usuario
     {
+        public Usuario()
+        {
+            Emprestimos = new List<Emprestimo>();
+            Reservas = new List<Reserva>();
+        }
+
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+        public List<Emprestimo> Emprestimos { get; set; }
+        public List<Reserva> Reservas { get; set; } // Adicionado
     }
 }
