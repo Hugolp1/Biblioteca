d02f74516f9dc8a2e13c0578e180406b0af57b3d
gabrprog <g.alberto.dev@gmail.com>
2024-06-30 13:53:57 -0300

    classe livro atualizada

--- Projeto/Models/Livro.cs
+++ Projeto/Models/Livro.cs
@@ -1,26 +1,33 @@
 ﻿using System;
-using Microsoft.EntityFrameworkCore.Query;
+using System.Collections.Generic;
 
-namespace Projeto.Models;
-
-public class Livro{
-    public Livro()
+namespace Projeto.Models
+{
+    public class Livro
     {
-        LivroId = Guid.NewGuid().ToString(); 
+        public Livro()
+        {
+            LivroId = Guid.NewGuid().ToString();
+            Emprestimos = new List<Emprestimo>();
+            Reservas = new List<Reserva>();
+        }
+
+        public Livro(string titulo, string genero)
+        {
+            LivroId = Guid.NewGuid().ToString();
+            Titulo = titulo;
+            Genero = genero;
+            Emprestimos = new List<Emprestimo>();
+            Reservas = new List<Reserva>();
+        }
+
+        public string LivroId { get; set; }
+        public string? Titulo { get; set; }
+        public string? Genero { get; set; }
+        public int AutorId { get; set; }
+        public int NumeroExemplares { get; set; }
+        public Autor? Autor { get; set; }
+        public List<Emprestimo> Emprestimos { get; set; }
+        public List<Reserva> Reservas { get; set; } // Adicionado
     }
-
-    public Livro(string titulo, string genero)
-    {
-        LivroId = Guid.NewGuid().ToString();
-        Titulo = titulo;
-        Genero = genero;
-    }
-
-    public string LivroId { get; set; }
-    public string? Titulo { get; set; }
-    public string? Genero { get; set; }
-    public int AutorId { get; set; }
-    public int NumeroExemplares { get; set; }
-
-    public Autor? Autor { get; set; }
 }
