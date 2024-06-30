5cdb3f105e38628b8861d58698a1a1f0e103fa3b
gabrprog <g.alberto.dev@gmail.com>
2024-06-30 13:58:08 -0300

    classe alterada

--- Projeto/Models/AppDbContext.cs
+++ Projeto/Models/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace Projeto.Models
         public DbSet<Autor> Autores { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Emprestimo> Emprestimos { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -28,6 +29,17 @@ namespace Projeto.Models
                 .HasOne(e => e.Usuario)
                 .WithMany(u => u.Emprestimos)
                 .HasForeignKey(e => e.UsuarioId);
+
+            // Configurações adicionais para Reserva
+            modelBuilder.Entity<Reserva>()
+                .HasOne(r => r.Livro)
+                .WithMany(l => l.Reservas)
+                .HasForeignKey(r => r.LivroId);
+
+            modelBuilder.Entity<Reserva>()
+                .HasOne(r => r.Usuario)
+                .WithMany(u => u.Reservas)
+                .HasForeignKey(r => r.UsuarioId);
         }
     }
 }
